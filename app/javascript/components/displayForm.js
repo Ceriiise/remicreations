b6276a6f1d95f7a1dd332147b91500bc5ec28684
Philippine Berton <Phi.berton@hotmail.fr>
2020-09-14 16:05:43 +0200

    add placeholder and some js

--- app/javascript/components/displayForm.js
+++ app/javascript/components/displayForm.js
@@ -2,10 +2,16 @@ const displayForm = () => {
   const newForm = document.getElementById('new-form');
   if(newForm) {
     const link = document.getElementById('new-form-link');
+    const text = document.querySelector('#new-form-link p small');
     const form = document.getElementById('new-form-input');
 
     link.addEventListener('click', (event) => {
       form.classList.toggle('d-none')
+      if (text.innerText === "ajouter une catégorie" || text.innerText === "ajoute ta première catégorie") {
+        text.innerText = "annuler"
+      } else {
+        text.innerText = "ajouter une catégorie";
+      }
     });
   }
 };
